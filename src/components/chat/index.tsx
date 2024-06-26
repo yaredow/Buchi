58a3f3770f8feff3d525dd7f176410ed8f2a41df
yaredow <yaredyilma11@gmail.com>
2024-06-26 18:03:30 +0300

    remove postgress and connect mongodb

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -29,39 +29,51 @@ export default function Chat() {
             </Avatar>
             <div className="flex flex-col">
               <span className="font-semibold">Mintu Mk</span>
-              <span className="text-sm text-gray-500">
+              <span className="text-sm text-slate-500 dark:text-slate-300">
                 Samsung Galaxy Note 20 Ultra 256 GB Black
               </span>
             </div>
           </div>
           <span className="text-green-600">ETB 36,000</span>
         </div>
-        <div className="flex-1 overflow-y-auto bg-gray-100 p-4">
+        <div className="flex-1 overflow-y-auto bg-slate-100 p-4">
           <div className="mb-4 flex justify-center">
-            <span className="text-sm text-gray-500">June, 26 2024</span>
+            <span className="text-sm text-slate-500 dark:text-slate-700">
+              June, 26 2024
+            </span>
           </div>
           <div className="mb-4 flex justify-start">
-            <div className="flex max-w-xs flex-col rounded-lg bg-white p-2 shadow">
+            <div className="flex max-w-xs flex-col rounded-lg bg-white p-2 shadow dark:bg-slate-600">
               <span>selam nw silkun lemegzat nbr</span>
-              <span className="self-end text-xs text-gray-500">13:58</span>
+              <span className="self-end text-xs text-slate-500 dark:text-slate-200">
+                13:58{" "}
+              </span>
             </div>
           </div>
           <div className="mb-4 flex justify-end">
-            <div className="flex max-w-xs flex-col rounded-lg bg-green-200 p-2 shadow">
+            <div className="flex max-w-xs flex-col rounded-lg bg-green-200 p-2 shadow dark:bg-green-400">
               <span>Hi Mintu, Ale</span>
-              <span className="self-end text-xs text-gray-500">14:06</span>
+              <span className="self-end text-xs text-slate-500 dark:text-slate-200">
+                14:06
+              </span>
             </div>
           </div>
           <div className="mb-4 flex justify-end">
-            <div className="flex max-w-xs flex-col rounded-lg bg-green-200 p-2 shadow">
+            <div className="flex max-w-xs flex-col rounded-lg bg-green-200 p-2 shadow dark:bg-green-400">
               <span>0928315616 bezih silk dewl</span>
-              <span className="self-end text-xs text-gray-500">15:25</span>
+              <span className="self-end text-xs text-slate-500 dark:text-slate-200">
+                15:25
+              </span>
             </div>
           </div>
         </div>
+
         <div className="flex items-center border-t p-4">
           <SmileIcon className="mr-4 h-6 w-6" />
-          <Input placeholder="Write your message here" className="flex-1" />
+          <Input
+            placeholder="Write your message here"
+            className="flex-1 bg-input text-secondary-article placeholder-secondary-article"
+          />
           <PaperclipIcon className="mx-4 h-6 w-6" />
           <MicIcon className="h-6 w-6" />
         </div>
--- src/components/chat/user-box.tsx
+++ src/components/chat/user-box.tsx
@@ -2,20 +2,24 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function UserBox() {
   return (
-    <div className="flex cursor-pointer items-center border-b p-4 hover:bg-gray-100">
+    <div className="group flex cursor-pointer items-center border-b p-4 hover:bg-gray-100 dark:text-slate-200 dark:hover:bg-gray-200 dark:hover:text-slate-800">
       <Avatar className="mr-4">
         <AvatarImage src="/placeholder-user.jpg" />
         <AvatarFallback>U</AvatarFallback>
       </Avatar>
       <div className="flex flex-1 flex-col">
-        <div className="flex justify-between">
-          <span className="font-semibold">እምባ በግ</span>
-          <span className="text-sm text-gray-500">24 Jun</span>
+        <div className="flex justify-between group-hover:text-slate-800">
+          <span className="font-semibold group-hover:text-slate-900">
+            Nahom A
+          </span>
+          <span className="text-sm text-slate-500 group-hover:text-gray-800">
+            24 Jun
+          </span>
         </div>
-        <span className="text-sm text-gray-500">
+        <span className="text-sm text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-700">
           Samsung Galaxy Note 20 Ultra 256 GB Black
         </span>
-        <span className="text-xs text-gray-400">
+        <span className="text-xs text-slate-400 group-hover:text-slate-500">
           Eshi last price snt new mecheresha yemtlw
         </span>
       </div>
