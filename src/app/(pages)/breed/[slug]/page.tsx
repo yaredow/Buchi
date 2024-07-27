45f959ec36b45f4f646be86d814002d42e0c6720
yaredow <yaredyilma11@gmail.com>
2024-07-27 20:00:02 +0300

    change the breed dynamic path to static

--- src/app/(pages)/breed/[slug]/page.tsx
+++ src/app/(pages)/breed/[slug]/page.tsx
@@ -1,4 +1,26 @@
 import BreedDetails from "@/components/breed/breed-details";
+import { getAllBreeds, getBreedWithSlug } from "@/data/breed";
+
+type IParams = {
+  slug: string;
+};
+
+export async function generateMetadta({ params }: { params: IParams }) {
+  const { slug } = params;
+  const breed = await getBreedWithSlug(slug);
+
+  return { title: `Breed ${breed?.breedName}` };
+}
+
+export async function generateStaticParams() {
+  const breeds = await getAllBreeds();
+
+  const slugs = breeds?.map((breed) => ({
+    slug: breed.slug,
+  }));
+
+  return slugs;
+}
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
