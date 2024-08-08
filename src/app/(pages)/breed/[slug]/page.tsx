ddfc13a6c0d12156114a5f7c727f3b61e84002b0
yaredow <yaredyilma11@gmail.com>
2024-08-08 12:20:14 +0300

    remove generate static param

--- src/app/(pages)/breed/[slug]/page.tsx
+++ src/app/(pages)/breed/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import BreedDetails from "@/components/breed/breed-details";
-import { getAllBreeds, getBreedWithSlug } from "@/data/breed";
+import { getBreedWithSlug } from "@/data/breed";
 import { Breed } from "@prisma/client";
 import { Metadata } from "next";
 
@@ -16,18 +16,6 @@ export async function generateMetadata({
   const breed = await getBreedWithSlug(slug);
 
   return { title: `Breed ${breed?.breedName}` };
-}
-
-export async function generateStaticParams() {
-  const breeds = await getAllBreeds();
-
-  if (!breeds) return [];
-
-  const slugs = breeds.map((breed) => ({
-    cabinId: String(breed.slug),
-  }));
-
-  return slugs;
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
